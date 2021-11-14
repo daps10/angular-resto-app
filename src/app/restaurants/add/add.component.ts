ec882c04678720d691992d2637467adbb648b071
Darpan Chavhan <darpanc@hyperlinkinfosystem.net.in>
2021-11-14 17:20:20 +0530

    - First imported "RestoService" and formControl and formGroup from the forms. - Created form binding with all which mentioned in the formGroup(). - addResto() function which is used to call addResto services and get data from that service. - during the fetch result time give support to showAlert and reset form which user filled.

--- src/app/restaurants/add/add.component.ts
+++ src/app/restaurants/add/add.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { RestoService } from '../../services/resto.service'
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
-
-  constructor() { }
+  showAlert:boolean = false;
+  // makes an instance of any
+  constructor(private resto: RestoService) { }
 
   ngOnInit(): void {
   }
 
+  public addResto = new FormGroup({
+    name : new FormControl(''),
+    email:new FormControl(''),
+    phone:new FormControl(''),
+    address:new FormControl(''),
+  });
+
+  addRestaurant(){
+    // console.log(this.addResto.value)
+    // subscribe work as a callback
+    this.resto.addResto(this.addResto.value).subscribe((result) => {
+      this.showAlert=true;
+      
+      // for reset form 
+      this.addResto.reset({})
+    });
+  }
+
+  // close alert function.
+  closeAlert(){
+    this.showAlert=false;
+  }
 }
